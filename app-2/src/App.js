d1a1097694329f133a4b5ce4a29ab6fa9dd3b8e4
mcintdre000 <38018234+mcintdre000@users.noreply.github.com>
2018-05-07 21:54:10 -0700

    Rendered app-2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      food: ['eggs','bacon','toast','nutella']
+    }
+
+
+  }
   render() {
+    let displayFood = this.state.food.map((e,index)=>{
+      return <h2 key={index}> {e} </h2>
+      
+    });
+    console.log(displayFood)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {displayFood}
       </div>
     );
   }
