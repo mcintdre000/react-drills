88852e348d4a1e97c4e8b356f4f79c3c057e46ac
mcintdre000 <38018234+mcintdre000@users.noreply.github.com>
2018-05-08 19:27:28 -0700

    Rendered app-8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
+const baseUrl = 'https://pokeapi.co/api/v2/'
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state= {
+      pokemon: []
+    }
+  }
+  
+  componentDidMount() {
+    console.log('will mount')
+    axios.get(`${baseUrl}pokemon/150/`).then(res => {
+      console.log(res);
+      this.setState({
+        pokemon: res.data
+      });
+    }).catch(err=>console.log(err))
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <h1> Pokemon: { this.state.pokemon.name }</h1>
+       <h1> Pokedex #: { this.state.pokemon.id }</h1>
+       <h1> Height: { this.state.pokemon.height }</h1>
+       <h1> Weight: { this.state.pokemon.weight }</h1>
       </div>
     );
   }
